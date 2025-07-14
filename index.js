4200bd45643384f54f30e8101c9b23b8173f0ebd
atmiya <vidhibhateliya14@gmail.com>
2025-07-14 09:30:42 +0530

    init with basic program

---
+++ index.js
@@ -0,0 +1,30 @@
+const nm="vidhi"
+console.log("hello"+nm)
+
+console.log("1. Even Odd Program")
+
+const a=6
+if(a%2)
+{
+    console.log("odd")
+}
+else
+{
+    console.log("even")
+}
+
+
+
+console.log("2. Maximum 2 Program")
+
+const a1=2
+const a2=4
+
+if(a1>a2)
+{
+    console.log(a1+"is maximum")
+}
+else
+{
+    console.log(a2+"is maximum")
+}
