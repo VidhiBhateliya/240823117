631b212b51880d56c58fa430be73c1c23783e414
atmiya <vidhibhateliya14@gmail.com>
2025-07-15 11:31:10 +0530

    program added

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-const nm="vidhi"
-console.log("hello"+nm)
+// const nm="vidhi"
+// console.log("hello"+nm)
 
-console.log("1. Even Odd Program")
+// console.log("1. Even Odd Program")
 
-const a=6
-if(a%2)
+// const a=6
+// if(a%2)
+// {
+//     console.log("odd")
+// }
+// else
+// {
+//     console.log("even")
+// }
+
+
+
+// console.log("2. Maximum 2 Program")
+
+// const a1=2
+// const a2=4
+
+// if(a1>a2)
+// {
+//     console.log(a1+"is maximum")
+// }
+// else
+// {
+//     console.log(a2+"is maximum")
+// }
+
+
+//console.log("3. Check Leap Year")
+
+
+//console.log(4. Sum of Natural Numbers)
+//console.log("5. Factorial of a Number")
+
+function fact(n)
 {
-    console.log("odd")
+    let a=1
+
+    for(let i=1;i<=n;i++)
+    {
+        a*=i;
+    }
+    return a
 }
-else
-{
-    console.log("even")
-}
-
-
-
-console.log("2. Maximum 2 Program")
-
-const a1=2
-const a2=4
-
-if(a1>a2)
-{
-    console.log(a1+"is maximum")
-}
-else
-{
-    console.log(a2+"is maximum")
-}
+console.log(fact(5))
